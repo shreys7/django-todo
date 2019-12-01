18c8198a152b7f6dd88d8653e73ddf021ddb542b
shreys7 <shreyshah97@gmail.com>
2019-12-01 23:18:13 +0530

    Created model Todo, made migrations and migrated to create entry in database

--- todoApp/todos/models.py
+++ todoApp/todos/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Todo(models.Model):
+    name=models.CharField(max_length=100)
+    created_date=models.DateField('Created')
+    deadline=models.DateField('Deadline')
+    description=models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
