98dc87cf679a0fb79f8f238c7f21ac4e7fc558d3
shreys7 <shreyshah97@gmail.com>
2019-12-01 23:42:37 +0530

    Added a sample gegeric listview for todo list

---
+++ todoApp/todos/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='Index')
+]
--- todoApp/todos/views.py
+++ todoApp/todos/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Todo
 
-# Create your views here.
+class IndexView(generic.ListView):
+    template_name = 'todos/index.html'
+    context_object_name = 'todo_list'
+
+    def get_queryset(self):
+        """Return all the latest todos."""
+        return Todo.objects.order_by('-created_date')
+
