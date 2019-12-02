b6e8039367afb89675718ea27c67b50bd3062a2f
shreys7 <shreyshah97@gmail.com>
2019-12-02 06:14:12 +0530

    Added urls.py and views.py inside todos app

--- todoApp/todos/urls.py
+++ todoApp/todos/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name='todos'
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='Index')
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:todo_id>/edit', views.edit, name='edit'),
+    path('<int:todo_id>/save', views.save, name='save')
 ]
--- todoApp/todos/views.py
+++ todoApp/todos/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views import generic
 from .models import Todo
+from django.http import HttpResponseRedirect
 
 class IndexView(generic.ListView):
     template_name = 'todos/index.html'
@@ -10,3 +11,22 @@ class IndexView(generic.ListView):
         """Return all the latest todos."""
         return Todo.objects.order_by('-created_at')
 
+def save(request, todo_id):
+
+    # form data from the request
+    title = request.POST.get('title')
+    description = request.POST.get('description')
+
+    todo = get_object_or_404(Todo, pk=todo_id)
+    todo.title = title
+    todo.description = description
+
+    todo.save()
+
+    return redirect('todos:index')
+
+def edit(request, todo_id):
+    todo = get_object_or_404(Todo, pk=todo_id)
+    return render(request, 'todos/form.html', {
+        'todo': todo
+    })
