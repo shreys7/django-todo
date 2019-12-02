e532e1d44cf6ed88186e560449a78f83625a8244
shreys7 <shreyshah97@gmail.com>
2019-12-02 06:13:32 +0530

    Updated attribute name from name to title of Todo model

--- todoApp/todos/models.py
+++ todoApp/todos/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 class Todo(models.Model):
-    name=models.CharField(max_length=100)
+    title=models.CharField(max_length=100)
     created_at=models.DateTimeField('Created', auto_now_add=True)
     update_at=models.DateTimeField('Updated', auto_now=True)
     description=models.CharField(max_length=200)
 
     def __str__(self):
-        return self.name
+        return self.title
