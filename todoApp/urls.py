a8faed361b23f6b6ba7eb1a531c02f9647d19deb
shreys7 <shreyshah97@gmail.com>
2019-12-05 00:42:58 +0530

    Added redirection of homepage to /todos endpoint

--- todoApp/urls.py
+++ todoApp/urls.py
@@ -17,10 +17,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from . import settings
+from . import settings, views
 
 urlpatterns = [
     path('todos/', include('todos.urls')),
     path('admin/', admin.site.urls),
+    path('', views.index)
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += staticfiles_urlpatterns()
