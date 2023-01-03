1a35fcd5bf903651baa67edc88519e93597540f3
root <root@ip-172-31-41-17.ap-northeast-1.compute.internal>
2023-01-03 10:04:26 +0000

    dockercompose

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
-FROM python3
+FROM python:3
 RUN pip install django==3.2
 COPY . .
 RUN python manage.py migrate
-CMD ["python","manage.py","runserver","0.0.0.0:8000"]
+CMD ["python","manage.py","runserver","0.0.0.0:8001"] 
+
--- todoApp/settings.py
+++ todoApp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '8)810zj@#^2xp=1=2rkozbv8#)gub6m1a^9qf&)d-9&x9*c2a_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'Asia/Kolkata'
+TIME_ZONE = 'Asia/Dhaka'
 
 USE_I18N = True
 
