c79b11a4734cccd9385bcf4cde3e2bb5d0cb7882
Subha Mishra <80328636+SubhaMishra1998@users.noreply.github.com>
2023-03-28 13:56:54 +0530

    allowed every host

--- todoApp/settings.py
+++ todoApp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '8)810zj@#^2xp=1=2rkozbv8#)gub6m1a^9qf&)d-9&x9*c2a_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['3.108.185.113']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
