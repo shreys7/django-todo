199774140f58506e77ac0cfe8d035b938af11c2a
Shubham S Dandekar <60695683+shubham-sd@users.noreply.github.com>
2023-09-16 20:14:51 +0530

    Update settings.py

--- todoApp/settings.py
+++ todoApp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '8)810zj@#^2xp=1=2rkozbv8#)gub6m1a^9qf&)d-9&x9*c2a_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
